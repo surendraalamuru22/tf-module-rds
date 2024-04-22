50aa695bc903aec5749fc232286aec85e73a3317
surendra <surendraredddyalamuru01@gmail.com>
2024-04-22 13:52:34 +0530

    rds

--- main.tf
+++ main.tf
@@ -7,16 +7,16 @@ resource "aws_rds_cluster" "default" {
   master_username     = "admin1"
   master_password     = "aheqrova"
   skip_final_snapshot = var.skip_final_snapshot
-  db_cluster_parameter_group_name = "${var.env}-${var.name}-roboshop-rds"
+#  db_cluster_parameter_group_name = "${var.env}-${var.name}-roboshop-rds"
   db_subnet_group_name   = "${var.env}-${var.name}-roboshop-rds"
 #  vpc_security_group_ids = [aws_security_group.sg.id]
 }
 
-resource "aws_rds_cluster_parameter_group" "default" {
-  name        = "${var.env}-${var.name}-roboshop-rds"
-  family      = "aurora5.6"
-  description = "RDS default cluster parameter group"
-}
+#resource "aws_rds_cluster_parameter_group" "default" {
+#  name        = "${var.env}-${var.name}-roboshop-rds"
+#  family      = "aurora5.6"
+#  description = "RDS default cluster parameter group"
+#}
 
 resource "aws_db_subnet_group" "default" {
   name       = "${var.env}-${var.name}-roboshop-rds"
